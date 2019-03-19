20709ab5a0dba45cfda0cced3fd31be1098ff099
Ahmed Kamel Taha <ahmedengu@gmail.com>
2019-03-19 15:18:11 +0200

    Update widget.js

--- widget.js
+++ widget.js
@@ -4,4 +4,39 @@
         if (!e.origin || e.origin.replace(/^https?:\/\//,'') !== "wuddrum.github.io" || !+data || data <= 0) return;
         document.getElementById('NewsWidget').style.height = data + 'px';
     });
+    
+    var newsWidget = document.getElementById('NewsWidget');
+    
+    // attach a listener to the iframe load
+    newsWidget.addEventListener("load", function () {
+        var newsWidgetHeight = newsWidget.clientHeight;
+        
+        // attach a listener to the page scroll
+        document.addEventListener('scroll', onScroll);
+
+        function inView() {
+            // check if in view
+            var windowHeight = window.innerHeight;
+            var scrollY = window.scrollY || window.pageYOffset;
+
+            var scrollPosition = scrollY + windowHeight;
+            var elementPosition = newsWidget.getBoundingClientRect().top + scrollY + newsWidgetHeight;
+
+            if (scrollPosition > elementPosition) {
+                return true;
+            }
+
+            return false;
+        }
+
+        function onScroll() {
+            // fire on page scroll
+            if (inView()) {
+                // when in view send a message to the iframe and remove the onScrolllistener 
+                console.log('onScroll_parent');
+                newsWidget.contentWindow.postMessage('onScroll', '*');
+                document.removeEventListener('scroll', onScroll);
+            }
+        }
+});
 }();
